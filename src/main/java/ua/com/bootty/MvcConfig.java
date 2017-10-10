3b144c56458c7175050bafd55b54978b0c1e338f
Alex <newbeecon@mail.ru>
2017-10-10 23:25:55 +0300

    Enabled DefaultServletHandlerConfigurer

--- src/main/java/ua/com/bootty/MvcConfig.java
+++ src/main/java/ua/com/bootty/MvcConfig.java
@@ -1,12 +1,17 @@
 package ua.com.bootty;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
+@ComponentScan
+@EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
@@ -16,11 +21,16 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     }
 
     @Bean
-    public InternalResourceViewResolver viewResolver() {
+    public InternalResourceViewResolver getViewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/webapp/WEB-INF/view/jsp/");
+        resolver.setPrefix("/WEB-INF/view/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 
 }
